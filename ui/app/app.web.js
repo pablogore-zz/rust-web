3c3452e4e4850d40bc31e911d0285f80c88b7088
pyros2097 <pyros2097@gmail.com>
2018-03-25 19:18:48 +0530

    remove app

--- ui/app/app.web.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { View, Text, Platform } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
-
-const App = () => (
-  <LoginScreen />
-);
-
-export default App;
--- ui/app/client.js
+++
@@ -1,8 +0,0 @@
-import { ApolloClient } from 'apollo-client';
-import { ApolloLink, execute, Observable } from 'apollo-link';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { withClientState } from 'apollo-link-state';
-import { createUploadLink } from 'apollo-upload-client';
-import { setContext } from 'apollo-link-context';
-import storageUtils from './utils/storage';
-import config from './config';
