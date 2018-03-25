c3d7d566a3196d964cbe365ad1e7dd72a68369dc
pyros2097 <pyros2097@gmail.com>
2018-03-25 19:19:12 +0530

    remive oldapp

--- ui/oldapp/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppRegistry } from 'react-native';
-import App from './app.web.js';
-
-AppRegistry.registerComponent('NCMApp', () => App);
-AppRegistry.runApplication('NCMApp', {
-  rootTag: document.getElementById('root'),
-});
